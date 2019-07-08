1a38c57d21ff081ec861a47b3955f45d9afa4694
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2019-07-08 17:51:55 +0200

    Remove relationship directive from final schema, #2

--- model/printer.go
+++ model/printer.go
@@ -1,11 +1,31 @@
 package model
 
 import (
+	"github.com/graphql-go/graphql/language/ast"
 	"github.com/graphql-go/graphql/language/printer"
 )
 
+func filterDirective(ds []*ast.Directive, name string) []*ast.Directive {
+	res := []*ast.Directive{}
+	for _, d := range ds {
+		if d.Name.Value != name {
+			res = append(res, d)
+		}
+	}
+	return res
+}
+
 // PrintSchema
 func PrintSchema(model Model) (string, error) {
+
+	for _, o := range model.Objects() {
+		fields := []*ast.FieldDefinition{}
+		for _, f := range o.Def.Fields {
+			f.Directives = filterDirective(f.Directives, "relationship")
+			fields = append(fields, f)
+		}
+		o.Def.Fields = fields
+	}
 
 	printed := printer.Print(model.Doc)
 	printedString, _ := printed.(string)
