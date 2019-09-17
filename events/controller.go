07bd4ba8f3ad097c5e1b9da7aac874884bf28ab1
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2019-09-17 12:22:45 +0200

    Add support for multiple EVENT_TRANSPORT_URL's

--- events/controller.go
+++ events/controller.go
@@ -2,8 +2,10 @@ package events
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	cloudevents "github.com/cloudevents/sdk-go"
 )
@@ -13,40 +15,46 @@ const (
 )
 
 type EventController struct {
-	client *cloudevents.Client
+	clients []cloudevents.Client
 }
 
 func NewEventController() (ec EventController, err error) {
-	URL := os.Getenv("EVENT_TRANSPORT_URL")
-	var _client *cloudevents.Client
-	if URL != "" {
-		t, tErr := cloudevents.NewHTTPTransport(
-			cloudevents.WithTarget(URL),
-			cloudevents.WithBinaryEncoding(),
-		)
-		err = tErr
-		if err != nil {
-			return
-		}
+	URLs := getENVArray("EVENT_TRANSPORT_URL")
+	_clients := []cloudevents.Client{}
+	for _, URL := range URLs {
+		if URL != "" {
+			t, tErr := cloudevents.NewHTTPTransport(
+				cloudevents.WithTarget(URL),
+				cloudevents.WithBinaryEncoding(),
+			)
+			err = tErr
+			if err != nil {
+				return
+			}
 
-		client, cErr := cloudevents.NewClient(t)
-		err = cErr
-		if err != nil {
-			return
+			client, cErr := cloudevents.NewClient(t)
+			err = cErr
+			if err != nil {
+				return
+			}
+			log.Printf("Created cloudevents client with target %s", URL)
+			_clients = append(_clients, client)
 		}
-		log.Printf("Created cloudevents client with target %s", URL)
-		_client = &client
 	}
-	ec = EventController{_client}
+	ec = EventController{_clients}
 	return
 }
 
 func (c *EventController) send(ctx context.Context, e cloudevents.Event) error {
-	if c.client == nil {
+	if c.clients == nil {
 		return nil
 	}
-	_, err := (*c.client).Send(ctx, e)
-	return err
+	for _, client := range c.clients {
+		if _, err := client.Send(ctx, e); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 // SendEvent ...
@@ -62,4 +70,23 @@ func (c *EventController) SendEvent(ctx context.Context, e *Event) (err error) {
 
 	err = c.send(ctx, event)
 	return
+}
+
+func getENVArray(name string) []string {
+	arr := []string{}
+
+	val := os.Getenv(name)
+	if val != "" {
+		arr = append(arr, strings.Split(val, ",")...)
+	}
+
+	for i := 0; i < 100; i++ {
+		key := fmt.Sprintf("%s_%d", name, i)
+		sval := os.Getenv(key)
+		if sval != "" {
+			arr = append(arr, sval)
+		}
+	}
+
+	return arr
 }
