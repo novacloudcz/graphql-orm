d20cce283dd617c56339ec51cf764dcf2496e2b4
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2019-10-09 10:34:41 +0200

    Fix dummy model

--- templates/dummy-model.go
+++ templates/dummy-model.go
@@ -1,18 +1,19 @@
 package templates
 
-var DummyModel = `type User {
-	email: String
-	firstName: String
-	lastName: String
+var DummyModel = `type User @entity {
+	email: String @column
+	firstName: String @column
+	lastName: String @column
 
 	tasks: [Task!]! @relationship(inverse:"assignee")
 }
 
-type Task {
-	title: String
-	completed: Boolean
-	dueDate: Time
+type Task @entity {
+	title: String @column
+	completed: Boolean @column
+	dueDate: Time @column
 
 	assignee: User @relationship(inverse:"tasks")
 }
+
 `
