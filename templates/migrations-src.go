1de59fe5c769d4058cbe4b64eb6e2bc8bdc8da5c
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2021-04-07 03:11:00 +0200

    Fix typo in migration comment

--- templates/migrations-src.go
+++ templates/migrations-src.go
@@ -19,7 +19,7 @@ func GetMigrations(db *gen.DB) []*gormigrate.Migration {
 				return db.AutoMigrate()
 			},
 			Rollback: func(tx *gorm.DB) error {
-				// there's not much we can do if initialization/automigration failes
+				// there's not much we can do if initialization/automigration fails
 				return nil
 			},
 		},
